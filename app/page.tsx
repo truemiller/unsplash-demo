eed494875caf838e664521523a9f4f69c5264cec
truemiller <mlxndrj@gmail.com>
2024-07-24 18:00:17 +0100

    feat: basic layout

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return <main className="flex flex-col">
+    <input type="text" />
+    <div id="toolbar"></div>
+    <section className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3"></section>
+    <div id="#pagination"></div>
+    </main>
+}
