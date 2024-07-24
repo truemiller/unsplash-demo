0eb0fbc181997931759110004c8adc605a9c252e
truemiller <mlxndrj@gmail.com>
2024-07-24 18:04:41 +0100

    chore: lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
-  return <main className="flex flex-col">
-    <input type="text" />
-    <div id="toolbar"></div>
-    <section className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3"></section>
-    <div id="#pagination"></div>
+  return (
+    <main className="flex flex-col">
+      <input type="text" />
+      <div id="toolbar"></div>
+      <section className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3"></section>
+      <div id="#pagination"></div>
     </main>
+  )
 }
