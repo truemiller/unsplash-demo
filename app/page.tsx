e0e6f042d7ff44b2a7a6809bb7cbdc054fe8db54
truemiller <mlxndrj@gmail.com>
2024-07-24 18:07:45 +0100

    feat: basic styling for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex flex-col">
+    <main className="container flex flex-col items-center gap-4">
       <input type="text" />
-      <div id="toolbar"></div>
+      <div id="toolbar" />
       <section className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3"></section>
-      <div id="#pagination"></div>
+      <div id="#pagination" />
     </main>
   )
 }
